b2806a230bfab93dd265abd19f6da45527b8e282
Neil Tiedeman <neilt@knowledgeservices.com>
2018-10-22 09:45:22 -0400

    Add project files.

---
+++ KomodoHires.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KomodoHires.WebMVC.MvcApplication" Language="C#" %>
---
+++ KomodoHires.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KomodoHires.WebMVC.Startup))]
+namespace KomodoHires.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
